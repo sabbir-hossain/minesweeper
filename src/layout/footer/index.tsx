e9004d8fce97089c382ea34cb56564bbac022064
sabbir-hossain <sabbir.hossain.dev@gmail.com>
2022-05-22 20:55:41 +0600

    update readme

--- src/layout/footer/index.tsx
+++ src/layout/footer/index.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div>
         <span className={styles.footerInfo}>Developed By: </span>
         <span className={styles.footerInfo}>S@BB!R</span>
-        <a href="https://github.com/sabbir-hossain/minesweeper" className={styles.footerLink}>source-code</a>
+        <a target={'_blank'} rel="noreferrer" href="https://github.com/sabbir-hossain/minesweeper" className={styles.footerLink}>source-code</a>
       </div>
     </footer>
   );
